a7d5265c6951e6815fd2cf6605a10b8e9a35d41e
Anatoly Belobrovik <anatoliybelobrovik@gmail.com>
2018-03-05 12:52:58 +0300

    Add array of  mock images

---
+++ src/Collections/api.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: '1',
+    src:
+      'http://chert-poberi.ru/wp-content/uploads/proga/111/images1/201712/igor5-17121714072507_5.jpg',
+    title: 'A kitty with a cup of coffee',
+  },
+  {
+    id: '2',
+    src: 'https://uc.lolkot.ru/picture/47363-37992-3.jpg',
+    title: 'A kitty with a cup of coffee',
+  },
+  {
+    id: '3',
+    src:
+      'http://s10.favim.com/orig/161123/animal-cat-coffee-kitty-Favim.com-4889090.jpeg',
+    title: 'A kitty with a cup of coffee',
+  },
+  {
+    id: '4',
+    src:
+      'http://scontent-arn2-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/22430171_134521763938677_674873313641627648_n.jpg',
+    title: 'A kitty with a cup of coffee',
+  },
+  {
+    id: '5',
+    src:
+      'https://78.media.tumblr.com/87d2659d6345c1e230c77081b8af003b/tumblr_ouc1k3npX61sojkzpo1_1280.jpg',
+    title: 'A kitty with a cup of coffee',
+  },
+  {
+    id: '6',
+    src:
+      'http://cdn2.arhivurokov.ru/viewImage.php?image=http://img0.liveinternet.ru/images/attach/d/0/138/501/138501004_132018492_tumblr_n3nvvjICtT1tv66qwo1_1280.jpg',
+    title: 'A kitty with a cup of coffee',
+  },
+];
