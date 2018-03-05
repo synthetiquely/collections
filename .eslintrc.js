cfe656d2ab733db001bf1f40d98a3b8d18748244
Anatoly Belobrovik <anatoliybelobrovik@gmail.com>
2018-03-05 12:52:28 +0300

    Fix eslint config error

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,6 @@ module.exports = {
   },
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    'react/prop-types': [false],
+    'react/prop-types': [0],
   },
 };
