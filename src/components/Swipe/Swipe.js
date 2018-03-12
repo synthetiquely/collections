4bc75e6e7a6c47052780874b02f953ed0baffd08
Anatoly Belobrovik <anatoliybelobrovik@gmail.com>
2018-03-12 15:52:19 +0300

    Refactor swipe event listeners

--- src/components/Swipe/Swipe.js
+++ src/components/Swipe/Swipe.js
@@ -10,7 +10,6 @@ class Swipe extends Component {
   constructor(props) {
     super(props);
     this.surface = null;
-    this.setRef = this.setRef.bind(this);
     this.state = {
       startX: 0,
       startY: 0,
@@ -22,10 +21,72 @@ class Swipe extends Component {
       startTime: 0,
       direction: 'none',
     };
+    this.setRef = this.setRef.bind(this);
+    this.onTouchStart = this.onTouchStart.bind(this);
+    this.onTouchMove = this.onTouchMove.bind(this);
+    this.onTouchEnd = this.onTouchEnd.bind(this);
   }
 
   componentDidMount() {
     this.initListeners();
+  }
+
+  onTouchStart(e) {
+    e.preventDefault();
+    const touchObj = e.changedTouches[0];
+    this.setState({
+      distX: 0,
+      distY: 0,
+      direction: 'none',
+      startX: touchObj.pageX,
+      startY: touchObj.pageY,
+      startTime: new Date().getTime(),
+    });
+  }
+
+  onTouchMove(e) {
+    e.preventDefault();
+    const touchObj = e.changedTouches[0];
+    const { startX, startY } = this.state;
+    const distX = touchObj.pageX - startX;
+    const distY = touchObj.pageY - startY;
+
+    if (Math.abs(distX) > Math.abs(distY)) {
+      this.setState({
+        distX,
+        distY,
+        direction: distX < 0 ? 'left' : 'right',
+      });
+    } else {
+      this.setState({
+        distX,
+        distY,
+        direction: distY < 0 ? 'up' : 'down',
+      });
+    }
+  }
+
+  onTouchEnd(e) {
+    e.preventDefault();
+
+    const {
+      startTime,
+      allowedTime,
+      distX,
+      distY,
+      threshold,
+      restraint,
+      direction,
+    } = this.state;
+
+    const elapsedTime = new Date().getTime() - startTime;
+    if (elapsedTime <= allowedTime) {
+      if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) {
+        this.handleSwipe(direction);
+      } else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint) {
+        this.handleSwipe(direction);
+      }
+    }
   }
 
   setRef(element) {
@@ -37,77 +98,9 @@ class Swipe extends Component {
   }
 
   initListeners() {
-    this.surface.addEventListener(
-      'touchstart',
-      (e) => {
-        e.preventDefault();
-        const touchObj = e.changedTouches[0];
-        this.setState({
-          distX: 0,
-          distY: 0,
-          direction: 'none',
-          startX: touchObj.pageX,
-          startY: touchObj.pageY,
-          startTime: new Date().getTime(),
-        });
-      },
-      false,
-    );
-    this.surface.addEventListener(
-      'touchmove',
-      (e) => {
-        e.preventDefault();
-        const touchObj = e.changedTouches[0];
-        const { startX, startY } = this.state;
-        const distX = touchObj.pageX - startX;
-        const distY = touchObj.pageY - startY;
-
-        if (Math.abs(distX) > Math.abs(distY)) {
-          this.setState({
-            distX,
-            distY,
-            direction: distX < 0 ? 'left' : 'right',
-          });
-        } else {
-          this.setState({
-            distX,
-            distY,
-            direction: distY < 0 ? 'up' : 'down',
-          });
-        }
-      },
-      false,
-    );
-
-    this.surface.addEventListener(
-      'touchend',
-      (e) => {
-        e.preventDefault();
-
-        const {
-          startTime,
-          allowedTime,
-          distX,
-          distY,
-          threshold,
-          restraint,
-          direction,
-        } = this.state;
-
-        const elapsedTime = new Date().getTime() - startTime;
-        if (elapsedTime <= allowedTime) {
-          if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) {
-            this.handleSwipe(direction);
-          } else if (
-            Math.abs(distY) >= threshold &&
-            Math.abs(distX) <= restraint
-          ) {
-            this.handleSwipe(direction);
-          }
-        }
-      },
-      false,
-    );
+    this.surface.addEventListener('touchstart', this.onTouchStart, false);
+    this.surface.addEventListener('touchmove', this.onTouchMove, false);
+    this.surface.addEventListener('touchend', this.onTouchEnd, false);
   }
 
   render() {
